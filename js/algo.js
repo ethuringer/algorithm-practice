f7f1206f077caf8dec7c06d60825f25baa5fcf4d
ethuringer <ethuringer@gmail.com>
2020-11-10 23:30:34 +0100

    practice 6

--- js/algo.js
+++ js/algo.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const numericArray1 = [215, 523, 531486, 4785, 457, 25, 4];
+
 
 // 1.
 /* function getMinElementFromAnArray(arr) {
@@ -15,10 +15,7 @@ const numericArray1 = [215, 523, 531486, 4785, 457, 25, 4];
 getMinElementFromAnArray(numericArray1); */
 
 // 2.
-
-/* const numericArray2 = [215, 523, 531486, 4785, 457, 25, 4];
-
-function getMaxElementFromAnArray(arr) {
+/* function getMaxElementFromAnArray(arr) {
     let max = arr[0];
     for (let i = 1; i < arr.length; i += 1) {
         if (arr[i] > max) {
@@ -27,7 +24,8 @@ function getMaxElementFromAnArray(arr) {
     }
     console.log(max);
 }
-getMaxElementFromAnArray(numericArray1); */
+getMaxElementFromAnArray(numericArray1);
+ */
 
 // 3.
 /* function getAverageOfArray(numericArray1) {
@@ -51,14 +49,63 @@ getAverageOfArray(numericArray1); */
 getSum(numericArray1);
  */
 
- // 5.
- /* function getEvenNumbers(numericArray1) {
-    let even = 0;
+// 5.
+/* function getEvenNumbers(numericArray1) {
+   let even = 0;
 for (let i = 0; i < numericArray1.length; i += 1) {
-    if (numericArray1[i] % 2 == 0) {
-        even++;
-    }
+   if (numericArray1[i] % 2 == 0) {
+       even++;
+   }
 }
 console.log(even);
+}
+getEvenNumbers(numericArray1); */
+
+// 6.
+
+/*  const numericArray1 = [999999,215, 523, 531486, 4785, 457, 25, 4];
+
+
+ function getSecondMinElementOfAnArray(numericArray1) {
+    let min = numericArray1[0];
+    let min2 = numericArray1[0];
+     for (let i = 0; i < numericArray1.length; i += 1)
+     {
+        if (numericArray1[i] < min) {
+        min2 = min
+        min = numericArray1[i];
+        } 
+        else   {
+        (numericArray1[i] < min2 && numericArray1[i] != min)
+        min2 = numericArray1[i]
+        }
+    }
+
+    console.log(min2);
  }
- getEvenNumbers(numericArray1); */
+ getSecondMinElementOfAnArray(numericArray1); */
+
+
+// 7.
+
+const numericArray1 = [215, 523, 531486, 4785, 457, 25, 4];
+
+
+function getThirdMaxElementOfAnArray(numericArray1) {
+    let max = numericArray1[0];
+    let max2 = numericArray1[0];
+    for (let i = 0; i < numericArray1.length; i += 1) {
+        if (numericArray1[i] > max) {
+            max2 = max
+            max = numericArray1[i];
+        }
+        else {
+            (numericArray1[i] > max2 && numericArray1[i] != max)
+            max2 = numericArray1[i];
+        }
+    }
+
+    console.log(max2);
+}
+getThirdMaxElementOfAnArray(numericArray1);
+
