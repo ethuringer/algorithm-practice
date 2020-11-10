d709dbe8210d6a258aa0619d874954628830ba4d
ethuringer <ethuringer@gmail.com>
2020-11-10 21:34:12 +0100

    functions from 1 to 5

--- js/algo.js
+++ js/algo.js
@@ -1,7 +1,9 @@
+'use strict';
+
 const numericArray1 = [215, 523, 531486, 4785, 457, 25, 4];
 
 // 1.
-function getMinElementFromAnArray(arr) {
+/* function getMinElementFromAnArray(arr) {
     let min = arr[0];
     for (let i = 1; i < arr.length; i += 1) {
         if (arr[i] < min) {
@@ -10,9 +12,12 @@ function getMinElementFromAnArray(arr) {
     }
     console.log(min);
 }
-getMinElementFromAnArray(numericArray1);
+getMinElementFromAnArray(numericArray1); */
 
 // 2.
+
+/* const numericArray2 = [215, 523, 531486, 4785, 457, 25, 4];
+
 function getMaxElementFromAnArray(arr) {
     let max = arr[0];
     for (let i = 1; i < arr.length; i += 1) {
@@ -22,4 +27,38 @@ function getMaxElementFromAnArray(arr) {
     }
     console.log(max);
 }
-getMaxElementFromAnArray(numericArray1);
+getMaxElementFromAnArray(numericArray1); */
+
+// 3.
+/* function getAverageOfArray(numericArray1) {
+    let sum = 0;
+    for (let i = 0; i < numericArray1.length; i += 1) {
+        sum += numericArray1[i];
+        
+    }
+    console.log(sum / numericArray1.length);
+}
+getAverageOfArray(numericArray1); */
+
+// 4.
+/* function getSum(numericArray1) {
+    let sum = 0;
+    for (let i = 0; i < numericArray1.length; i += 1) {
+        sum += numericArray1[i];
+    }
+    console.log(sum);
+}
+getSum(numericArray1);
+ */
+
+ // 5.
+ /* function getEvenNumbers(numericArray1) {
+    let even = 0;
+for (let i = 0; i < numericArray1.length; i += 1) {
+    if (numericArray1[i] % 2 == 0) {
+        even++;
+    }
+}
+console.log(even);
+ }
+ getEvenNumbers(numericArray1); */
